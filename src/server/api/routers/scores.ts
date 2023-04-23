ed0096975b5ded805c6af152e666767c1d14fb98
Lawrence Yolland <lawrence.yolland@ericsson.com>
2023-04-23 20:03:41 +0100

    update schema and start landing page

---
+++ src/server/api/routers/scores.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const scoreRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.score.findMany();
+  }),
+});
+
